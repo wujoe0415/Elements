21ddf9b7d2e4809f1ef234ae48612b30f484956b
wujoe0415 <wujoe0415@gmail.com>
2024-11-03 17:08:19 +0800

    add fire extinction

--- Assets/Scripts/Interaction/Fire/Burn.cs
+++ Assets/Scripts/Interaction/Fire/Burn.cs
@@ -9,12 +9,23 @@ public class Burn : MonoBehaviour
 
     private void Awake()
     {
-        BurnTime = Random.Range(2f, 5f);
+        BurnTime = Random.Range(20f, 50f);
         StartCoroutine(Burning());
+    }
+    public void OnParticleCollision(GameObject other)
+    {
+        Debug.Log(other.name);
+        if (other.name == "FX_Rain_Collision_01")
+        {
+            transform.localScale -= Vector3.one * 0.1f;
+            if (transform.localScale.x < 0.2f)
+                Destroy(gameObject);
+        }
     }
     private IEnumerator Burning()
     {
        yield return new WaitForSeconds(BurnTime);
+        Debug.Log("Burned");
        Destroy(gameObject);
     }
 }
