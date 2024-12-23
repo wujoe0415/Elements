4ea30e95815d8cfdc29ae9a84e9955f6c1c0b20a
wujoe0415 <wujoe0415@gmail.com>
2024-12-23 11:19:46 +0800

    temp build

--- Assets/Scripts/Dialogue/OnTriggerDialogue.cs
+++ Assets/Scripts/Dialogue/OnTriggerDialogue.cs
@@ -6,8 +6,16 @@ public class OnTriggerDialogue : MonoBehaviour
 {
     public List<string> Dialogues = new List<string>();
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            OnTrigger();
+        }
+    }
     public void OnTrigger()
     {
+        Debug.Log("trigger");
         DialogueSystem.Instance.SetDiagues(Dialogues);
         DialogueSystem.Instance.StartDialogue();
     }
